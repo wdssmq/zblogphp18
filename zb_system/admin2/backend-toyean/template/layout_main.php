18bcdc9a67e9593f9c4ef92647bed5af405267d0
zx.asd <zhuzhuxf@gmail.com>
2025-12-29 20:08:01 +0800

    改了文章管理;

--- zb_system/admin2/backend-toyean/template/layout_main.php
+++ zb_system/admin2/backend-toyean/template/layout_main.php
@@ -2,7 +2,7 @@
       <div class="content">
         <div class="title">
           <h2>{$main->Header} <ul id="topmenu">{if $main.Action=='admin'}{ResponseAdmin_TopMenu()}{/if}{$main->SubMenu}</ul></h2>
-          <p>欢迎回来，{$zbp->user->LevelName}！这里是您网站的概览信息</p>
+          {if $main.Header=='admin'}<p>欢迎回来，管理员！这里是您网站的概览信息</p>{/if}
         </div>
 {php}$zbp->GetHint();{/php}
 {php}HookFilterPlugin('Filter_Plugin_Admin_Hint');{/php}
