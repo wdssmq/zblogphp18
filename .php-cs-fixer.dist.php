2d290f7f24c4ef7df873ef8965c23a6fe560d375
wdssmq <wdssmq@qq.com>
2026-01-01 00:20:49 +0800

    chore: 更新 php-cs-fixer 配置，修复文件路径处理和规则设置

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
-return Config::create()
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect()) // @TODO 4.0 no need to call this manually
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PhpCsFixer' => true,
-        '@PhpCsFixer:risky' => true,
+        '@auto' => true,
+        '@auto:risky' => true,
+        '@PhpCsFixer:risky' => true
     ])
+    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
     ->setFinder(
-        Finder::create()
+        (new Finder())
+            // 💡 root folder to check
             ->in(__DIR__)
+            // 💡 additional files, eg bin entry file
+            // ->append([__DIR__.'/bin-entry-file'])
+            // 💡 folders to exclude, if any
             ->exclude(['node_modules'])
+            // 💡 path patterns to exclude, if any
+            // ->notPath([/* ... */])
+            // 💡 extra configs
+            // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
+            // ->ignoreVCS(true) // true by default
     )
 ;
