036c47e7f0b25d490fcec7ab5d984621cd2df338
zx.asd <zhuzhuxf@gmail.com>
2026-01-01 20:54:05 +0800

    伪静态管理模板制作;

--- zb_system/defend/backend/RewriteMng.php
+++ zb_system/defend/backend/RewriteMng.php
@@ -3,25 +3,87 @@
 {php}<?php
 function OutputOptionItemsOfUrl($type)
 {
-    global $ua, $zbp;
-    $s = '';
-    $d = 'style="display:none;"';
-    if ($zbp->option['ZC_STATIC_MODE'] == 'ACTIVE' || strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') !== false) {
-        $r = 'disabled="disabled"';
-    } else {
-        $r = '';
-    }
+  $ua = array(
+      'ZC_ARTICLE_REGEX' => array(
+          '{%host%}?id={%id%}',
+          '{%host%}index.php/post/{%id%}.html',
+          '{%host%}post/{%id%}.html',
+          '{%host%}post/{%alias%}.html',
+          '{%host%}{%year%}/{%month%}/{%id%}/',
+          '{%host%}{%category%}/{%alias%}/',
+      ),
 
-    $i = 0;
-    foreach ($ua[$type] as $key => $value) {
-        $s .= '<p ' . $d . '><label><input ' . $r . ' type="radio" name="radio' . $type . '" value="' . $value . '" onclick="$(\'#' . $type . '\').val($(this).val())" />&nbsp;' . $value . '</label></p>';
-        $i++;
-        if ($i > 1) {
-            $d = '';
-        }
-    }
+      'ZC_PAGE_REGEX' => array(
+          '{%host%}?id={%id%}',
+          '{%host%}index.php/{%id%}.html',
+          '{%host%}{%id%}.html',
+          '{%host%}{%alias%}.html',
+          '{%host%}{%alias%}/',
+          //'{%host%}{%alias%}',
+      ),
 
-    echo $s;
+      'ZC_INDEX_REGEX' => array(
+          '{%host%}?page={%page%}',
+          '{%host%}index.php/page_{%page%}.html',
+          '{%host%}page_{%page%}.html',
+          '{%host%}page_{%page%}/',
+          //'{%host%}page_{%page%}',
+          '{%host%}page/{%page%}/',
+      ),
+
+      'ZC_CATEGORY_REGEX' => array(
+          '{%host%}?cate={%id%}&page={%page%}',
+          '{%host%}index.php/category-{%id%}_{%page%}.html',
+          '{%host%}category-{%id%}_{%page%}.html',
+          '{%host%}category-{%alias%}_{%page%}.html',
+          '{%host%}category/{%alias%}/{%page%}/',
+          '{%host%}category/{%id%}/{%page%}/',
+      ),
+
+      'ZC_TAGS_REGEX' => array(
+          '{%host%}?tags={%id%}&page={%page%}',
+          '{%host%}index.php/tags-{%id%}_{%page%}.html',
+          '{%host%}tags-{%id%}_{%page%}.html',
+          '{%host%}tags-{%alias%}_{%page%}.html',
+          '{%host%}tags/{%alias%}/{%page%}/',
+      ),
+
+      'ZC_DATE_REGEX' => array(
+          '{%host%}?date={%date%}&page={%page%}',
+          '{%host%}index.php/date-{%date%}_{%page%}.html',
+          '{%host%}date-{%date%}_{%page%}.html',
+          '{%host%}post/{%date%}_{%page%}.html',
+          '{%host%}date/{%date%}/{%page%}/',
+      ),
+
+      'ZC_AUTHOR_REGEX' => array(
+          '{%host%}?auth={%id%}&page={%page%}',
+          '{%host%}index.php/author-{%id%}_{%page%}.html',
+          '{%host%}author-{%id%}_{%page%}.html',
+          '{%host%}author/{%id%}/{%page%}/',
+          '{%host%}author/{%alias%}/{%page%}/',
+      ),
+  );
+
+  global $zbp;
+  $s = '';
+  $d = 'style="display:none;"';
+  if ($zbp->option['ZC_STATIC_MODE'] == 'ACTIVE' || strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') !== false) {
+      $r = 'disabled="disabled"';
+  } else {
+      $r = '';
+  }
+
+  $i = 0;
+  foreach ($ua[$type] as $key => $value) {
+      $s .= '<p ' . $d . '><label><input ' . $r . ' type="radio" name="radio' . $type . '" value="' . $value . '" onclick="$(\'#' . $type . '\').val($(this).val())" />&nbsp;' . $value . '</label></p>';
+      $i++;
+      if ($i > 1) {
+          $d = '';
+      }
+  }
+
+  echo $s;
 }
 
 $csrfToken = $zbp->GetCSRFToken();
@@ -33,70 +95,70 @@ $csrfToken = $zbp->GetCSRFToken();
 <tr>
     <th class="td20"><p align='left'><b>·静态化选项</b><br><span class='note'>&nbsp;&nbsp;使用伪静态前必须确认主机是否支持</span></p></th>
     <th>
-<p><label><input type="radio" <?php echo $zbp->option['ZC_STATIC_MODE'] == 'ACTIVE' ? 'checked="checked"' : ''?> value="ACTIVE" name="ZC_STATIC_MODE" onchange="changeOptions(0);" /> &nbsp;&nbsp;动态</label>
+<p><label><input type="radio" {trim($option['ZC_STATIC_MODE'] == 'ACTIVE' ? 'checked="checked"'  : '')} value="ACTIVE" name="ZC_STATIC_MODE" onchange="changeOptions(0);" /> &nbsp;&nbsp;动态</label>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<label><input type="radio" <?php echo !($zbp->option['ZC_STATIC_MODE'] == 'REWRITE' && strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') === false) ? '' : 'checked="checked"'?>  value="REWRITE"  name="ZC_STATIC_MODE" onchange="changeOptions(2);" />&nbsp;&nbsp;伪静态</label>
+<label><input type="radio" {trim( !($option['ZC_STATIC_MODE'] == 'REWRITE' && strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') === false) ? '' : 'checked="checked"' )}  value="REWRITE"  name="ZC_STATIC_MODE" onchange="changeOptions(2);" />&nbsp;&nbsp;伪静态</label>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<label><input type="radio" <?php echo !($zbp->option['ZC_STATIC_MODE'] == 'REWRITE' && strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') !== false) ? '' : 'checked="checked"'?>  value="REWRITE"  name="ZC_STATIC_MODE" onchange="changeOptions(1);" />&nbsp;&nbsp;index.php式仿伪静态</label>
+<label><input type="radio" {trim( !($option['ZC_STATIC_MODE'] == 'REWRITE' && strpos($zbp->option['ZC_ARTICLE_REGEX'], '{%host%}index.php') !== false) ? '' : 'checked="checked"' )}  value="REWRITE"  name="ZC_STATIC_MODE" onchange="changeOptions(1);" />&nbsp;&nbsp;index.php式仿伪静态</label>
 
 </p>
     </th>
     </tr>
 <tr>
     <td><p align='left'><b>·文章的URL配置</b></p></td>
-    <td><input id='ZC_ARTICLE_REGEX' name='ZC_ARTICLE_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_ARTICLE_REGEX']?>'></td>
+    <td><input id='ZC_ARTICLE_REGEX' name='ZC_ARTICLE_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_ARTICLE_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_ARTICLE_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_ARTICLE_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·页面的URL配置</b></p></td>
-    <td><input id='ZC_PAGE_REGEX' name='ZC_PAGE_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_PAGE_REGEX']?>'></td>
+    <td><input id='ZC_PAGE_REGEX' name='ZC_PAGE_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_PAGE_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_PAGE_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_PAGE_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·首页的URL配置</b></p></td>
-    <td><input id='ZC_INDEX_REGEX' name='ZC_INDEX_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_INDEX_REGEX']?>'></td>
+    <td><input id='ZC_INDEX_REGEX' name='ZC_INDEX_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_INDEX_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_INDEX_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_INDEX_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·分类页的URL配置</b></p></td>
-    <td><input id='ZC_CATEGORY_REGEX' name='ZC_CATEGORY_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_CATEGORY_REGEX']?>'></td>
+    <td><input id='ZC_CATEGORY_REGEX' name='ZC_CATEGORY_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_CATEGORY_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_CATEGORY_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_CATEGORY_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·标签页的URL配置</b></p></td>
-    <td><input id='ZC_TAGS_REGEX' name='ZC_TAGS_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_TAGS_REGEX']?>'></td>
+    <td><input id='ZC_TAGS_REGEX' name='ZC_TAGS_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_TAGS_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_TAGS_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_TAGS_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·日期页的URL配置</b></p></td>
-    <td><input id='ZC_DATE_REGEX' name='ZC_DATE_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_DATE_REGEX']?>'></td>
+    <td><input id='ZC_DATE_REGEX' name='ZC_DATE_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_DATE_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_DATE_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_DATE_REGEX')}</td>
 </tr>
 <tr>
     <td><p align='left'><b>·作者页的URL配置</b></p></td>
-    <td><input id='ZC_AUTHOR_REGEX' name='ZC_AUTHOR_REGEX' style='width:500px;' type='text' value='<?php echo $zbp->option['ZC_AUTHOR_REGEX']?>'></td>
+    <td><input id='ZC_AUTHOR_REGEX' name='ZC_AUTHOR_REGEX' style='width:500px;' type='text' value='{$zbp->option['ZC_AUTHOR_REGEX']}'></td>
 </tr>
 <tr>
     <td></td>
-    <td><?php OutputOptionItemsOfUrl('ZC_AUTHOR_REGEX')?></td>
+    <td>{OutputOptionItemsOfUrl('ZC_AUTHOR_REGEX')}</td>
 </tr>
 <?php
 
@@ -104,8 +166,7 @@ $csrfToken = $zbp->GetCSRFToken();
 </table>
       <hr/>
       <p>
-        1· 规则可以自定义，请注意如果规则解析过于广泛会覆盖之后的规则，浏览页面时就会出现故障.
-        <br/>2· index.php式仿伪静态在Apache,IIS下可以不用生成伪静态规则.
+        <a href="https://docs.zblogcn.com/php/#/books/start-30-rewrite" target="_blank"><i class="icon-flag-fill" style="font-size:small; margin-right: 0.2em;"></i>伪静态配置指南</a>
       </p>
       <p>
         <input type="submit" class="button" value="{$lang['msg']['submit']}" />
