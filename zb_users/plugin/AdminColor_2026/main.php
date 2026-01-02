c6c430da54c6427da041c583d9270b6104da640d
wdssmq <wdssmq@qq.com>
2026-01-03 00:06:38 +0800

    feat: 添加注释以增强代码可读性

--- zb_users/plugin/AdminColor_2026/main.php
+++ zb_users/plugin/AdminColor_2026/main.php
@@ -1,21 +1,25 @@
 <?php
+// 引入系统
 require '../../../zb_system/function/c_system_base.php';
 require '../../../zb_system/function/c_system_admin_function.php';
 require '../../../zb_system/admin2/function/admin2_function.php';
 
-$zbp->ismanage = true;
+// 加载系统
 $zbp->Load();
 
+// 验证权限
 $action = 'root';
 if (!$zbp->CheckRights($action)) {
     $zbp->ShowError(6);
     die();
 }
+// 插件启用验证
 if (!$zbp->CheckPlugin('AdminColor_2026')) {
     $zbp->ShowError(48);
     die();
 }
 
+// 显示内容构建
 $blogtitle = '后台配色器_2026';
 $content = $zbp->template_admin->Output("plugin_AdminColor_2026_Content");
 $ActionInfo = (object) [
@@ -29,6 +33,7 @@ $ActionInfo = (object) [
     'Content' => $content,
 ];
 
+// 输出页面
 $zbp->template_admin->SetTags('title', $ActionInfo->Title);
 $zbp->template_admin->SetTags('main', $ActionInfo);
 $zbp->template_admin->Display('index');
