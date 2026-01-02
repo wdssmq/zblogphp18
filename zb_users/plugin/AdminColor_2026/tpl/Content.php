afe3fa696ab4a6bf6e6ebfffee4f992f7143ac2e
GitHub Action <action@github.com>
2026-01-02 16:35:10 +0000

    Auto Commit - PhpCsFixer

--- zb_users/plugin/AdminColor_2026/tpl/Content.php
+++ zb_users/plugin/AdminColor_2026/tpl/Content.php
@@ -1,7 +1,7 @@
-<?php die();?>
+<?php exit(); ?>
 
 
- <form action="{php}<?php echo BuildSafeURL("main.php?act=save");?>{/php}" method="post">
+ <form action="{php}<?php echo BuildSafeURL('main.php?act=save'); ?>{/php}" method="post">
    <table width="100%" class="tableBorder table_striped">
    <tr>
      <th width="10%">项目</th>
@@ -10,27 +10,27 @@
    </tr>
    <tr>
      <td>标准色</td>
-     <td>{php}<?php zbpform::text("NormalColor", $zbp->Config("AdminColor_2026")->NormalColor, "90%"); ?>{/php}</td>
+     <td>{php}<?php zbpform::text('NormalColor', $zbp->Config('AdminColor_2026')->NormalColor, '90%'); ?>{/php}</td>
      <td></td>
    </tr>
    <tr>
      <td>深色</td>
-     <td>{php}<?php zbpform::text("BlodColor", $zbp->Config("AdminColor_2026")->BlodColor, "90%"); ?>{/php}</td>
+     <td>{php}<?php zbpform::text('BlodColor', $zbp->Config('AdminColor_2026')->BlodColor, '90%'); ?>{/php}</td>
      <td></td>
    </tr>
    <tr>
      <td>浅色</td>
-     <td>{php}<?php zbpform::text("LightColor", $zbp->Config("AdminColor_2026")->LightColor, "90%"); ?>{/php}</td>
+     <td>{php}<?php zbpform::text('LightColor', $zbp->Config('AdminColor_2026')->LightColor, '90%'); ?>{/php}</td>
      <td></td>
    </tr>
    <tr>
      <td>高光色</td>
-     <td>{php}<?php zbpform::text("HighColor", $zbp->Config("AdminColor_2026")->HighColor, "90%"); ?>{/php}</td>
+     <td>{php}<?php zbpform::text('HighColor', $zbp->Config('AdminColor_2026')->HighColor, '90%'); ?>{/php}</td>
      <td></td>
    </tr>
    <tr>
      <td>反色</td>
-     <td>{php}<?php zbpform::text("AntiColor", $zbp->Config("AdminColor_2026")->AntiColor, "90%"); ?>{/php}</td>
+     <td>{php}<?php zbpform::text('AntiColor', $zbp->Config('AdminColor_2026')->AntiColor, '90%'); ?>{/php}</td>
      <td></td>
    </tr>
    <tr>
