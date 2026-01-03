eb6a752f0bbd8e32e3257aabc9dd9d5124c372b9
wdssmq <wdssmq@qq.com>
2026-01-03 23:07:11 +0800

    重构 UEditor 插件，优化脚本引入方式，新增脚本文件以提升代码可维护性

--- zb_users/plugin/UEditor/include.php
+++ zb_users/plugin/UEditor/include.php
@@ -35,94 +35,25 @@ function UninstallPlugin_UEditor()
 function ueditor_addscript_begin()
 {
     global $zbp;
-    echo '<script type="text/javascript" src="' . $zbp->host . 'zb_users/plugin/UEditor/ueditor.config.php"></script>';
-    echo '<script type="text/javascript" src="' . $zbp->host . 'zb_users/plugin/UEditor/ueditor.all.min.js"></script>';
+    echo '<script src="'. UEditor_Path('ueditor.config.php', 'host') .'"></script>';
+    echo '<script src="'. UEditor_Path('ueditor.all.min.js', 'host') .'"></script>';
     echo '<style type="text/css">#editor_content{height:auto}</style>';
 }
 
 function ueditor_addscript_end()
 {
-    global $zbp;
-
-    $s = <<<js
-<script type="text/javascript">
-
-var EditorIntroOption = {
-	toolbars:[['Source', 'bold', 'italic','link','insertimage','Undo', 'Redo']],
-	autoHeightEnabled:false,
-	initialFrameHeight:200
+    echo '<script src="'. UEditor_Path('script',"host") .'"></script>';
 }
 
-
-function getContent(){
-  return editor_api.editor.content.get();
-}
-
-function getIntro(){
-  return editor_api.editor.intro.get();
-}
-
-function setContent(s){
-	editor_api.editor.content.put(s);
-}
-
-function setIntro(s){
-  editor_api.editor.intro.put(s);
-}
-
-function editor_init(){
-
-    function addButton(id){
-        var s=this;
-        UE.registerUI(s.name, function(editor, uiName) {
-            return new UE.ui.Button({
-                name: uiName,
-                title: uiName,
-                cssRules: "background: rgba(0, 0, 0, 0) url("+s.icon+") no-repeat center / 16px 16px !important;",
-                onclick: function() {
-                    s.callback.call(editor)
-                }
-            });
-        },void 0,id);
-    }
-    
-    typeof contentBarBtn === 'undefined' || $.each(contentBarBtn, function(index, obj){
-        UEDITOR_CONFIG["toolbars"][0].push(obj.name);
-        addButton.call(obj,'editor_content');
-    })
-    
-    typeof introBarBtn === 'undefined' || $.each(introBarBtn, function(index, obj){
-        EditorIntroOption.toolbars[0].push(obj.name);
-        addButton.call(obj,'editor_intro');
-    })
-        
-    editor_api.editor.content.obj=UE.getEditor('editor_content');
-    editor_api.editor.intro.obj=UE.getEditor('editor_intro',EditorIntroOption);
-    editor_api.editor.content.get=function(){return this.obj.getContent()};
-    editor_api.editor.content.put=function(str){return this.obj.setContent(str)};
-    editor_api.editor.content.focus=function(){return this.obj.focus()};
-    editor_api.editor.content.insert=function(str){return this.obj.execCommand("insertHtml", str)};
-    editor_api.editor.intro.get=function(){return this.obj.getContent()};
-    editor_api.editor.intro.put=function(str){return this.obj.setContent(str)};
-    editor_api.editor.intro.focus=function(){return this.obj.focus()};
-    editor_api.editor.intro.insert=function(str){return this.obj.execCommand("insertHtml", str)};
-    
-    
-    editor_api.editor.content.obj.ready(function(){sContent=editor_api.editor.content.get();});
-    editor_api.editor.intro.obj.ready(function(){sIntro=editor_api.editor.intro.get();});
-    
-    $(document).ready(function(){
-    	$('#edit').submit(function(){if(editor_api.editor.content.obj.queryCommandState('source')==1) editor_api.editor.content.obj.execCommand('source');
-    	if(editor_api.editor.intro.obj.queryCommandState('source')==1) editor_api.editor.intro.obj.execCommand('source');})
-    	/*源码模式下保存时必须切换*/
-    
-    
-    	if (bloghost != "/" && (bloghost).indexOf(location.host.toLowerCase()) < 0)
-    		alert("您设置了域名固化，请使用" + bloghost + "访问或进入后台修改域名，否则图片无法上传。");
-    });
-
-}
-</script>
-js;
-    echo $s;
+function UEditor_Path($file, $t = 'path')
+{
+  global $zbp;
+  $result = $zbp->$t . 'zb_users/plugin/UEditor/';
+  switch ($file) {
+    case 'script':
+      return $result . 'script/script.js';
+      break;
+    default:
+      return $result . $file;
+  }
 }
---
+++ zb_users/plugin/UEditor/script/script.js
@@ -0,0 +1,75 @@
+const EditorIntroOption = {
+  toolbars: [['Source', 'bold', 'italic', 'link', 'insertimage', 'Undo', 'Redo']],
+  autoHeightEnabled: false,
+  initialFrameHeight: 200
+}
+
+
+function getContent() {
+  return editor_api.editor.content.get();
+}
+
+function getIntro() {
+  return editor_api.editor.intro.get();
+}
+
+function setContent(s) {
+  editor_api.editor.content.put(s);
+}
+
+function setIntro(s) {
+  editor_api.editor.intro.put(s);
+}
+
+function editor_init() {
+  function addButton(id) {
+    const s = this;
+    UE.registerUI(s.name, function(editor, uiName) {
+      return new UE.ui.Button({
+        name: uiName,
+        title: uiName,
+        cssRules: "background: rgba(0, 0, 0, 0) url(" + s.icon + ") no-repeat center / 16px 16px !important;",
+        onclick: function() {
+          s.callback.call(editor)
+        }
+      });
+    }, void 0, id);
+  }
+
+  typeof contentBarBtn === 'undefined' || $.each(contentBarBtn, function(index, obj) {
+    UEDITOR_CONFIG["toolbars"][0].push(obj.name);
+    addButton.call(obj, 'editor_content');
+  })
+
+  typeof introBarBtn === 'undefined' || $.each(introBarBtn, function(index, obj) {
+    EditorIntroOption.toolbars[0].push(obj.name);
+    addButton.call(obj, 'editor_intro');
+  })
+
+  editor_api.editor.content.obj = UE.getEditor('editor_content');
+  editor_api.editor.intro.obj = UE.getEditor('editor_intro', EditorIntroOption);
+  editor_api.editor.content.get = function() { return this.obj.getContent() };
+  editor_api.editor.content.put = function(str) { return this.obj.setContent(str) };
+  editor_api.editor.content.focus = function() { return this.obj.focus() };
+  editor_api.editor.content.insert = function(str) { return this.obj.execCommand("insertHtml", str) };
+  editor_api.editor.intro.get = function() { return this.obj.getContent() };
+  editor_api.editor.intro.put = function(str) { return this.obj.setContent(str) };
+  editor_api.editor.intro.focus = function() { return this.obj.focus() };
+  editor_api.editor.intro.insert = function(str) { return this.obj.execCommand("insertHtml", str) };
+
+  editor_api.editor.content.obj.ready(function() { sContent = editor_api.editor.content.get(); });
+  editor_api.editor.intro.obj.ready(function() { sIntro = editor_api.editor.intro.get(); });
+
+  $(document).ready(function() {
+    $('#edit').submit(function() {
+      if (editor_api.editor.content.obj.queryCommandState('source') == 1) editor_api.editor.content.obj.execCommand('source');
+      if (editor_api.editor.intro.obj.queryCommandState('source') == 1) editor_api.editor.intro.obj.execCommand('source');
+    })
+    /* 源码模式下保存时必须切换 */
+
+
+    if (bloghost != "/" && (bloghost).indexOf(location.host.toLowerCase()) < 0)
+      alert("您设置了域名固化，请使用" + bloghost + "访问或进入后台修改域名，否则图片无法上传。");
+  });
+
+}
