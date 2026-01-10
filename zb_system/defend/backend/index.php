cdad1e608e555ef427e8354e328bad5d3bb4ff3d
wdssmq <wdssmq@qq.com>
2026-01-11 00:14:30 +0800

    后台模板-新：使用新的 c_admin_js_add.js 并修改脚本顺序

--- zb_system/defend/backend/index.php
+++ zb_system/defend/backend/index.php
@@ -14,9 +14,9 @@
     <script src="{$host}zb_system/script/jquery-2.2.4.min.js?v={$version}"></script>
     <script src="{$host}zb_system/script/jquery-ui.custom.min.js?v={$version}"></script>
     <script src="{$host}zb_system/script/zblogphp.js?v={$version}"></script>
-     <script>
+    <script>
         window.__ADMIN_JS_CONFIG_URL__ = '{$zbp.ajaxurl}admin2';
-     </script>
+    </script>
     <script src="{$host}zb_system/admin2/script/c_admin_js_add.js?v={$version}"></script>
     <script src="{$host}zb_system/admin2/{$backendtheme}/script/{$backendtheme}.js?v={$version}"></script>
     {$header}
