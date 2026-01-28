00dbc00a6dbc592f3ced56b82cd491848b259319
GitHub Action <action@github.com>
2026-01-28 09:48:21 +0000

    Auto Commit - PhpCsFixer

--- zb_system/function/lib/base/module.php
+++ zb_system/function/lib/base/module.php
@@ -16,7 +16,7 @@ if (!defined('ZBP_PATH')) {
  */
 abstract class Base__Module extends Base
 {
-    public $private_links = null;
+    public $private_links;
 
     /**
      * 构造函数.
@@ -113,6 +113,7 @@ abstract class Base__Module extends Base
                     $this->ParseLink();
                 }
             }
+
             return $this->private_links;
         }
         if ('ContentWithoutId' == $name) {
